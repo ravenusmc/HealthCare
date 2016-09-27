6b1be449eb26a3435e975e61aa4d751ef80acf55
ravenusmc <mcuddy77@gmail.com>
2016-09-27 17:45:43 -0400

    added more comments for planning

--- idea.py
+++ idea.py
@@ -5,6 +5,14 @@ import matplotlib.pyplot as plt
 
 #First goal: Make a function that tells the user the lowest cost by zip code
 
+#Will need to ask the person for their zip
+#Will need to ask the person for their medical condition
+#I may want to have show the user what type of medical conditions that come up with what they want because 
+#there many matches may come up. Thus the user will be presented with a list based on what they typed in 
+#Which will further be refined. 
+#Then I will take those two pieces of information and show them locations near them. 
+
+
 
 # health_data = pd.read_csv("inpatientCharges.csv")
 # print(health_data.head())
