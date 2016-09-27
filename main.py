247a5dfe57c127db00d6fae38c49aea6298d4f01
ravenusmc <mcuddy77@gmail.com>
2016-09-27 13:27:17 -0400

    set up main menu function

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 #This is the main file for the program. 
+
+def main():
+        print("-------------------------------------------------")
+        print("------Welcome to HealthCare Cost Information-----")
+        print("Our Goal is to help you find cost effective care!")
+        print("-------------------------------------------------")
+        input("Press Enter for the main menu")
+
+def main_menu():
+    print("1. ")
