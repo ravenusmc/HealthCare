4ba9256514c7027a5196f52dd6a626f1ea30b41a
ravenusmc <mcuddy77@gmail.com>
2016-10-01 15:14:06 -0400

    added comments

--- main.py
+++ main.py
@@ -27,18 +27,26 @@ def main_menu():
     if choice == 1:
         cheapestCost()
 
+#This function finds the cheapest cost based on the area and the condition the indiviudal enters. However, 
+#the problem is that the condition uses the DRG codes which are medical/technical terms and I need a way
+#That the lay person can type in a condition and it is matched up to the DRG code.
 def cheapestCost():
+    #The data is pulled and set equal to health_data
     health_data = pd.read_csv("inpatientCharges.csv")
     print("\033c")
     state = input("What is your state: ")
     issue = input("What is your issue: ")
     cost = int(input("What is the cost that you are looking for: "))
+    #Here the data will be sorted out to only find the information for that state the user wants to see.
     state_data = health_data[(health_data.ProviderState == state)]
+    #This further seperatates out the data for only what the user wants to see.
     issue_data = state_data[state_data.DRGDefinition.str.contains(issue.upper())]
+    #At this point I have to convert a column to a float, it is an object, and then I have to get rid of the $ sign.
+    #Once that is done, the data will be pulled that is below a specific value.
     matches = issue_data[issue_data.AverageTotalPayments.str.extract(r'.*?(\d+\.*\d*)', expand=False).astype(float) <= cost]
     print("Here are the matches for your criteria: ")
+    #The data is displayed which matches the state, issue and less than cost. 
     print(matches)
-
     quit_main_menu()
 
 ### Functions that are not critical to the running of the program:
